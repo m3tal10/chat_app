e3df74b4712c2705558dd4fda25e75358722ed4e
Mashrafie <rahimsheikh05@gmail.com>
2024-10-28 23:27:56 +0600

    Added local chat host functionality.

--- chat_app/chats/models.py
+++ chat_app/chats/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    room_name= models.CharField(max_length=255)
+    message= models.TextField(blank=True, null=True)
